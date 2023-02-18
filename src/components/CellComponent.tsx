09a3a78e69a48b24e9f223105a7d6a306a762b47
Aleksandr Novikov <65459275+novikov-main-acc@users.noreply.github.com>
2023-02-19 01:39:25 +1000

    board

---
+++ src/components/CellComponent.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Cell } from '../models/Cell';
+
+interface CellProps{
+    cell: Cell
+}
+
+const CellComponent: React.FC<CellProps> = ({cell}) => {
+    return (
+        <div
+        className={['cell', cell.color].join(' ')}
+        >
+         {cell.figure}   
+        </div>
+    );
+};
+
+export default CellComponent;
