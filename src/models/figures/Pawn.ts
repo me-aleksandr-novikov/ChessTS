092e573630415c680a0924fd019800a800da35bb
Aleksandr Novikov <65459275+novikov-main-acc@users.noreply.github.com>
2023-02-19 22:03:24 +1000

    figuressteps

--- src/models/figures/Pawn.ts
+++ src/models/figures/Pawn.ts
@@ -5,9 +5,30 @@ import blackLogo from '../../assets/black-pawn.png'
 import whiteLogo from '../../assets/white-pawn.png'
 
 export class Pawn extends Figure {
+
+    isFirstStep: boolean = true;
+
     constructor(color: Colors, cell: Cell){
         super(color, cell);
         this.logo = color === Colors.BLACK ? blackLogo : whiteLogo;
         this.name = FigureNames.PAWN;
+    }
+
+    canMove(target: Cell): boolean {
+        if(!super.canMove(target)){
+            return false;    
+        }
+        const direction = this.cell.figure?.color === Colors.BLACK ? 1 : -1
+        const firstStepDirection = this.cell.figure?.color === Colors.BLACK ? 2 : -2
+
+        if((target.y === this.cell.y + direction || this.isFirstStep
+            && (target.y === this.cell.y + firstStepDirection))
+            && target.x === this.cell.x
+            && this.cell.board.getCell(target.x, target.y).isEmpty()){
+                this.isFirstStep 
+                return true;
+        }
+
+        return false;
     }
 }
